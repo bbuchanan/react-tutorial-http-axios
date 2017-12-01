6d8a13d27457e83e3b5db0a24475b92d8cb3823c
Bill Buchanan <bbuchanan@gmail.com>
2017-12-01 12:45:18 -0500

    loading components via routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-//import axios from 'axios'
-import axios from '../../axios'
 import Posts from './Posts/Posts'
+import { Route } from 'react-router-dom'
 
 import './Blog.css';
 
@@ -18,7 +17,9 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Posts />
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+        <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+        <Route path="/" exact component={Posts} />
       </div>
     );
   }
