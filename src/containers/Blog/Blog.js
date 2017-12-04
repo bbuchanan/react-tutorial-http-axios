ce56930d8bcddc996db59fb6fc8e3849eb593901
Bill Buchanan <bbuchanan@gmail.com>
2017-12-04 11:10:55 -0500

    converted Link to NavLink to style the active links

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost'
-import { Route, Link } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 
 import './Blog.css';
 
@@ -13,17 +13,19 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to={{
+              <li><NavLink
+                activeClassName="my-active"
+                activeStyle={{color: '#fa923f',
+              textDecoration: 'underline'}}
+                exact to="/">Home</NavLink></li>
+              <li><NavLink to={{
                 pathname: '/new-post',
                 hash: '#submit',
                 search: '?quick-submit=true'
-              }}>New Post</Link></li>
+              }}>New Post</NavLink></li>
               </ul>
           </nav>
         </header>
-        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
-        <Route path="/" render={() => <h1>Home 2</h1>} /> */}
         <Route path="/" exact component={Posts} />
         <Route path="/new-post" component={NewPost} />
       </div>
