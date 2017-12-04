4fd42b6145f0722f8b70d9743fa61604e5a2394a
Bill Buchanan <bbuchanan@gmail.com>
2017-12-04 10:50:26 -0500

    Replaced a href with Link component from react-router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts'
-import { Route } from 'react-router-dom'
+import NewPost from './NewPost/NewPost'
+import { Route, Link } from 'react-router-dom'
 
 import './Blog.css';
 
@@ -12,14 +13,19 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/new-post">New Post</a></li>
-            </ul>
+              <li><Link to={{ pathname: '/'}}>Home</Link></li>
+              <li><Link to={{
+                pathname: '/new-post',
+                hash: '#submit',
+                search: '?quick-submit=true'
+              }}>New Post</Link></li>
+              </ul>
           </nav>
         </header>
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
         <Route path="/" render={() => <h1>Home 2</h1>} /> */}
         <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
       </div>
     );
   }
