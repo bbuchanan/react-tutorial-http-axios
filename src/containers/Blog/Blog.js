7ba0a27d5497b18c3c47ef8dc0bfca5fc232ad1d
Bill Buchanan <bbuchanan@gmail.com>
2017-12-04 11:31:37 -0500

    Made the various components route parameter aware.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost'
 import { Route, NavLink } from 'react-router-dom'
+import FullPost from './FullPost/FullPost'
 
 import './Blog.css';
 
@@ -28,6 +29,7 @@ class Blog extends Component {
         </header>
         <Route path="/" exact component={Posts} />
         <Route path="/new-post" component={NewPost} />
+        <Route path="/:id" exact component={FullPost} />
       </div>
     );
   }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import axios from '../../../axios'
 import Post from '../../../components/Post/Post'
 import classes from './Posts.css'
+import { Link } from 'react-router-dom'
 
 class Posts extends Component {
   state = {
@@ -36,10 +37,13 @@ class Posts extends Component {
     let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>
     if (!this.state.error) {
       posts = this.state.posts.map(post => {
-        return <Post key={post.id}
-          title={post.title}
-          clicked={(id) => this.postSelectedHandler(post.id)}
-          author={post.author} />
+        return (
+        <Link key={post.id} to={'/' + post.id}>
+            <Post
+              title={post.title}
+              clicked={(id) => this.postSelectedHandler(post.id)}
+              author={post.author} />
+        </Link>)
       })
     }
     return (
