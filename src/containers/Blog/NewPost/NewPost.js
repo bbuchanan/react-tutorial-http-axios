31fbc8ce7ec58fdb69b12bf6e6bdc8e10846f4a1
Bill Buchanan <bbuchanan@gmail.com>
2017-12-04 12:45:21 -0500

    redirect using props.history.push/replace

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -23,7 +23,8 @@ class NewPost extends Component {
     axios.post('/posts', post)
       .then(response => {
         console.log(response)
-        this.setState({submitted: true})
+        this.props.history.push("/")
+        //this.setState({submitted: true})
       })
   }
   render() {
@@ -33,7 +34,7 @@ class NewPost extends Component {
     }
     return (
       <div className="NewPost">
-        {redirect}>
+        {redirect}
         <h1>Add a Post</h1>
         <label>Title</label>
         <input type="text" value={this.state.title} onChange={(event) => this.setState({ title: event.target.value })} />
