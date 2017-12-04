dcfa1d74a50b729b8349d9f9478823388e18a6c5
Bill Buchanan <bbuchanan@gmail.com>
2017-12-04 13:11:30 -0500

    Created higher order component to asynchronously and lazily load components as directed

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts'
-import NewPost from './NewPost/NewPost'
+// import NewPost from './NewPost/NewPost'
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
-
+import asyncComponent from '../../hoc/asyncComponent'
 import './Blog.css';
+
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost')
+})
+
 
 class Blog extends Component {
   state = {
-    auth: false
+    auth: true
   }
 
   render() {
@@ -30,7 +35,7 @@ class Blog extends Component {
           </nav>
         </header>
         <Switch>
-          {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+          {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
           <Route path="/posts" component={Posts} />
           <Route render={() => <h1>Not found</h1>} />
           {/* <Redirect from="/" to="/posts" /> */}
